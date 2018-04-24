70f7adf88fb313441b17c6fd635d755a28834cf1
Erick Vieira <erick-vieira-s@outlook.com>
2018-04-24 11:32:29 -0300

    Create rascunho.sql

---
+++ querys/rascunho.sql
@@ -0,0 +1,31 @@
+USE empresa;
+SELECT	pnome
+FROM	projeto
+		JOIN trabalha_em 	ON pno = pnumero
+		JOIN departamento	ON departamento.dnumero = dnum
+        JOIN empregado 		ON dno = dnumero
+WHERE	empregado.dno = departamento.dnumero
+		AND projeto.dnum = departamento.dnumero
+GROUP	BY	pnome
+ORDER	BY	pnome;
+
+SELECT	*
+FROM	departamento;
+
+SELECT	pnome
+FROM	trabalha_em
+		JOIN empregado		ON essn = empregado.ssn
+        JOIN departamento	ON empregado.dno = departamento.dnumero
+        JOIN projeto		ON pno = projeto.pnumero
+GROUP	BY	pnome
+ORDER	BY	pnome;
+        
+SELECT	*
+FROM	empregado
+ORDER	BY	pnome;
+
+SELECT	*
+FROM	depto_localizacoes;
+
+SELECT	*
+FROM	projeto;
